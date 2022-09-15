db2b8dd5289a36afa5d697229285120811836736
PaoV00 <vangg.ppao@gmail.com>
2022-09-14 19:56:41 -0500

    Testing

---
+++ ICS372/src/ICS372/DealerGroup.java
@@ -0,0 +1,48 @@
+package ICS372;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.json.*;
+
+public class DealerGroup {
+
+	private Dealership[] dealers;
+	
+	public JSONObject exportDealerVehicles() {
+		JSONObject file = new JSONObject();
+		
+		for(Dealership dealer : dealers) {
+			for(Entry<Integer, Vehicle> vehicle : dealer.getVehicleInventory().entrySet()) {
+				try {
+					file.put("dealership_id", vehicle.getValue().getDealershipID());
+					file.put("vehicle_type", vehicle.getValue().getVehicleType());
+					file.put("vehicle_manufacturer", vehicle.getValue().getVehicleManufacturer());
+					file.put("vehicle_model", vehicle.getValue().getVehicleModel());
+					file.put("vehicle_id", vehicle.getValue().getVehicleID());
+					file.put("price", vehicle.getValue().getPrice());
+					file.put("acquisition_date", vehicle.getValue().getAcquisitionDate());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return file;
+	}
+	
+	public String displayDealerVehicles() {
+		for(Dealership dealer : dealers) {
+			for(Vehicle vehicle : dealer.getVehicleInventory().values()) {
+				System.out.println(vehicle);
+			}
+		}
+		return null;
+	}
+	
+	public String getDealer() {
+		for(Dealership dealer: dealers) {
+			System.out.println(dealer);
+		}
+		return null;
+	}
+}
