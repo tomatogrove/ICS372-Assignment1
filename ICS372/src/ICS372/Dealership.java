da178bfdc1612c287b106b5a667788a354eb48f1
PaoV00 <vangg.ppao@gmail.com>
2022-09-14 19:38:28 -0500

    test

---
+++ ICS372/src/ICS372/Dealership.java
@@ -0,0 +1,63 @@
+package ICS372;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Dealership {
+    private int dealerID;
+    private boolean vehicleAcquisition;
+    private Map<Integer, Vehicle> vehicleInventory;
+    private int vehicleKey = 0;
+
+    
+    //getters
+    public int getDealerID() {
+        return dealerID;
+    }
+
+    public boolean isVehicleAcquisition() {
+        return vehicleAcquisition;
+    }
+
+    public Map<Integer, Vehicle> getVehicleInventory() {
+        return vehicleInventory;
+    }
+
+    public Dealership(int newDealer) {
+        dealerID = newDealer;
+        vehicleAcquisition = true;
+        vehicleInventory = new HashMap<>();
+    }
+    //constructors
+    public Dealership(int dealerID, boolean vehicleAcquisition, HashMap<Integer, Vehicle> vehicleInventory) {
+        this.dealerID = dealerID;
+        this.vehicleAcquisition = vehicleAcquisition;
+        this.vehicleInventory = vehicleInventory;
+    }
+    //methods
+    public void addIncomingVehicle(Vehicle car) {
+        if(vehicleAcquisition == true){
+        this.vehicleInventory.put(vehicleKey, car);
+        vehicleKey++;
+        } else {
+            System.out.println("This dealer is not allowed to add additional vehicles to their inventory");
+        }
+    }
+
+    public void enableDealerVehicleAcquisition(Dealership dealer) {
+        dealer.vehicleAcquisition = true;
+    }
+
+    public void disableDealerVehicleAcquisition(Dealership dealer) {
+        dealer.vehicleAcquisition = false;
+    }
+
+    @Override
+    public String toString() {
+        String inventory = "";
+        for( Map.Entry<Integer, Vehicle> car : this.vehicleInventory.entrySet()) {
+            inventory += car.getKey() + "\n_______________\nVehicle ID: " + car.getValue().getVehicleID() + "\nDealership ID: " + car.getValue().getDealershipID() + "\nVehicle Type: " + car.getValue().getVehicleType() + "\nVehicle Model: " + car.getValue().getVehicleModel() + "\nVehicle Manufacturer: " + car.getValue().getVehicleManufacturer() + "\nPrice: $" + car.getValue().getPrice() + "\nAcquisition Date: " + car.getValue().getAcquisitionDate() + "\n";
+        }
+        return inventory;
+    }
+}
