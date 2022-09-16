8222c3013f9086b362c192a4ef54c7b936b454ad
CwongM <calvinywong@gmail.com>
2022-09-16 17:38:12 -0500

    added newlines to system out messages

--- src/Dealership.java
+++ src/Dealership.java
@@ -38,12 +38,12 @@ public class Dealership {
             if(vehicleAcquisition == true && this.vehicleInventory.containsKey(stockNumber) == false){
                 this.vehicleInventory.put(stockNumber, car);
             } else if(vehicleAcquisition == false){
-                System.out.println("This dealer is not allowed to add additional vehicles to their inventory");
+                System.out.println("This dealer is not allowed to add additional vehicles to their inventory\n");
             } else if(this.vehicleInventory.containsKey(stockNumber) == true) {
-                System.out.println("This vehicle is already in this dealer's inventory");
+                System.out.println("This vehicle is already in this dealer's inventory\n");
             }
         } else {
-            System.out.println("Not a valid vehicle type.");
+            System.out.println("Not a valid vehicle type.\n");
         }
     }
 
