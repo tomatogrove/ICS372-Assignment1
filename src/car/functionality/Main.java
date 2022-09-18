f332e839db9f6b730cde1ae3866fcecfe7949b6a
tomatogrove <66643214+tomatogrove@users.noreply.github.com>
2022-09-18 16:27:54 -0500

    trying to use the simple parser

--- src/car/functionality/Main.java
+++ src/car/functionality/Main.java
@@ -3,9 +3,12 @@ package car.functionality;
 import car.inventory.DealerGroup;
 import car.inventory.Dealership;
 import iabGUI.Vehicle;
+import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -36,19 +39,22 @@ public class Main {
         }
     }
 
-    private static boolean checkCommand(String command, JacksonVehicleJSONParser json, DealerGroup dealerGroup) {
+    private static boolean checkCommand(String command, JacksonVehicleJSONParser jacksonParser, DealerGroup dealerGroup) {
         String[] commandAndParameter = command.split(" ", 2);
         Dealership dealer;
 
         switch (commandAndParameter[0]) {
             case "addIncomingVehicles":
-                List<Vehicle> vehicles = json.read(commandAndParameter[1]);
+                List<Vehicle> vehicles = jacksonParser.read(commandAndParameter[1]);
                 dealerGroup.addIncomingVehicles(vehicles);
+
+                Main.tryAddVehicles(commandAndParameter, dealerGroup);
+
                 break;
             case "exportDealerVehicles":
                 dealer = getDealer(dealerGroup, commandAndParameter);
                 if(dealer != null) {
-                    json.write(dealer);
+                    jacksonParser.write(dealer);
                 }
                 break;
             case "displayDealerVehicles":
@@ -73,6 +79,19 @@ public class Main {
                 break;
         }
         return true;
+    }
+
+    //would be better if it was a vehicle from the beginning. This is an unsafe cast.
+    private static void tryAddVehicles(String[] commandAndParameter, DealerGroup dealerGroup) {
+        try {
+            VehicleJSONParser simpleParser = new VehicleJSONParser(commandAndParameter[1]);
+            List<Vehicle> simpleVehicles = simpleParser.buildMap().values().stream()
+                    .map((vehicle) -> (Vehicle) vehicle)
+                    .collect(Collectors.toList());
+            dealerGroup.addIncomingVehicles(simpleVehicles);
+        } catch (IOException | ParseException e) {
+            System.out.println("Input file is invalid or inaccessible");
+        }
     }
 
     private static Dealership getDealer(DealerGroup dealerGroup, String[] commandAndParameter) {
